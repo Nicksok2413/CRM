43da7b26d508380e47dd8a642c7e8e6820af69ad
nick <8650180292@my.lerna.by>
2025-10-07 13:57:48 +0300

    use decouple add apps

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 
 # Application definition
@@ -34,6 +36,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Приложения
+    'apps.advertisements.apps.AdvertisementsConfig',
+    'apps.common.apps.CommonConfig',
+    'apps.contracts.apps.ContractsConfig',
+    'apps.customers.apps.CustomersConfig',
+    'apps.leads.apps.LeadsConfig',
+    'apps.products.apps.ProductsConfig',
+    'apps.users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
