c6682bf09e06d2b849ec5c3c8281d5d33332811a
nick <8650180292@my.lerna.by>
2025-10-07 15:37:58 +0300

    add AUTH_USER_MODEL add ALLOWED_HOSTS, INTERNAL_IPS

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,23 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "0.0.0.0",
+]
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+    "0.0.0.0",
+]
+
+if DEBUG:
+    import socket
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS.append("10.0.2.2")
+    INTERNAL_IPS.extend(
+        [ip[: ip.rfind(".")] + ".1" for ip in ips]
+    )
 
 # Application definition
 
@@ -128,3 +145,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ======================================================================
+
+# Указываем Django использовать кастомную модель пользователя
+AUTH_USER_MODEL = 'users.User'
