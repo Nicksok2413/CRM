6aaea7f6e504bc6d8f833307d2c98215b12bfd87
nick <8650180292@my.lerna.by>
2025-10-08 16:59:20 +0300

    add leads admin

--- apps/leads/admin.py
+++ apps/leads/admin.py
@@ -1,3 +1,28 @@
+"""
+Настройки административной панели для приложения leads.
+"""
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import PotentialClient
+
+
+@admin.register(PotentialClient)
+class PotentialClientAdmin(admin.ModelAdmin):
+    """
+    Административный класс для модели PotentialClient.
+    """
+    # Поля для отображения в списке всех лидах, включая связанную рекламную компанию.
+    list_display = ('last_name', 'first_name', 'email', 'phone', 'ad_campaign')
+
+    # Оптимизация запросов: при загрузке списка лидов
+    # сразу загружаем связанные данные рекламных компаний одним SQL-запросом, избегая проблемы "N+1".
+    # `list_select_related` заставляет Django использовать SQL JOIN, получая все данные за один запрос.
+    list_select_related = ('ad_campaign',)
+
+    # Поиск по всем контактным данным.
+    search_fields = ('last_name', 'first_name', 'email', 'phone')
+
+    # Фильтры.
+    # Фильтрация по ForeignKey (`ad_campaign`) создаст список всех рекламных компаний для выбора.
+    list_filter = ('ad_campaign',)
