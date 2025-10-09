ed3c9b1389ec79f96b54f12c3c815a0686bc2a77
nick <8650180292@my.lerna.by>
2025-10-09 15:06:11 +0300

    add AdCampaign creation form

---
+++ apps/advertisements/forms.py
@@ -0,0 +1,17 @@
+"""
+Формы для приложения advertisements.
+"""
+
+from django import forms
+
+from .models import AdCampaign
+
+
+class AdCampaignForm(forms.ModelForm):
+    """
+    Форма для создания и редактирования Рекламных кампаний.
+    """
+
+    class Meta:
+        model = AdCampaign
+        fields = ('name', 'service', 'channel', 'budget')
