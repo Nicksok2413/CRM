becd36278e131ddd68d6616208f6b313a15a57b4
nick <8650180292@my.lerna.by>
2025-10-09 12:33:39 +0300

    add form for creating new product(service)

---
+++ apps/products/forms.py
@@ -0,0 +1,19 @@
+"""
+Формы для приложения products.
+"""
+
+from django import forms
+
+from .models import Service
+
+
+class ServiceForm(forms.ModelForm):
+    """
+    Форма для создания и редактирования Услуг.
+
+    Автоматически генерирует поля на основе модели Service и обеспечивает валидацию данных.
+    """
+
+    class Meta:
+        model = Service
+        fields = ('name', 'description', 'cost')
