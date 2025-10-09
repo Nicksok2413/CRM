61a79c71ee8ce740d8f1ae66f9676c0ab06d62e3
nick <8650180292@my.lerna.by>
2025-10-09 16:42:53 +0300

    add leads urls

---
+++ apps/leads/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from .views import (
+    LeadCreateView,
+    LeadDeleteView,
+    LeadDetailView,
+    LeadListView,
+    LeadUpdateView,
+)
+
+# Пространство имен для URL-адресов этого приложения
+# Позволит использовать, например, `{% url 'leads:list' %}` в шаблонах
+app_name = 'leads'
+
+urlpatterns = [
+    path('', LeadListView.as_view(), name='list'),
+    path('new/', LeadCreateView.as_view(), name='create'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='detail'),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='delete'),
+    path('<int:pk>/edit/', LeadUpdateView.as_view(), name='edit'),
+]
