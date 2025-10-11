b7d909ed14c1ade88f39f3ca94d8f07de881f9cd
nick <nicksok2413@gmail.com>
2025-10-11 14:21:27 +0300

    add customers urls

--- apps/customers/urls.py
+++ apps/customers/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
 
-from .views import ActiveClientCreateView
-
+from .views import (
+    ActiveClientCreateFromLeadView,
+    ActiveClientDetailView,
+    ActiveClientDeleteView,
+    ActiveClientListView,
+    ActiveClientUpdateView,
+)
 
 # Пространство имен для URL-адресов этого приложения
 # Позволит использовать, например, `{% url 'customers:list' %}` в шаблонах
 app_name = 'customers'
 
 urlpatterns = [
+    path('', ActiveClientListView.as_view(), name='list'),
+    path('<int:pk>/', ActiveClientDetailView.as_view(), name='detail'),
+    path('<int:pk>/delete/', ActiveClientDeleteView.as_view(), name='delete'),
+    path('<int:pk>/edit/', ActiveClientUpdateView.as_view(), name='edit'),
+
     # URL для создания активного клиента из лида
-    path('new/from-lead/<int:lead_pk>/', ActiveClientCreateView.as_view(), name='create_from_lead'),
+    path('new/from-lead/<int:lead_pk>/', ActiveClientCreateFromLeadView.as_view(), name='create_from_lead'),
 ]
