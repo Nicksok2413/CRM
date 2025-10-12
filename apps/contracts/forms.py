b0ac09ebe3667448b933dbec14e0cc792efce11e
nick <nicksok2413@gmail.com>
2025-10-12 17:10:49 +0300

    add flatpickr calendar

--- apps/contracts/forms.py
+++ apps/contracts/forms.py
@@ -3,8 +3,10 @@
 """
 
 from django import forms
+from django.core.validators import FileExtensionValidator
 
 from .models import Contract
+from apps.common.validators import validate_document_size
 
 
 class ContractForm(forms.ModelForm):
@@ -18,6 +20,26 @@ class ContractForm(forms.ModelForm):
 
         # Для полей дат добавляем виджеты с выбором даты, чтобы улучшить UX.
         widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date'}),
-            'end_date': forms.DateInput(attrs={'type': 'date'}),
+            'start_date': forms.DateInput(
+                attrs={'placeholder': 'Выберите дату...', 'class': 'datepicker'}
+            ),
+            'end_date': forms.DateInput(
+                attrs={'placeholder': 'Выберите дату...', 'class': 'datepicker'}
+            ),
         }
+
+    def clean(self):
+        """
+        Переопределяем метод clean для добавления кастомной логики валидации,
+        которая затрагивает несколько полей.
+        """
+        cleaned_data = super().clean()
+        start_date = cleaned_data.get("start_date")
+        end_date = cleaned_data.get("end_date")
+
+        # Проверяем, что дата окончания не раньше даты начала
+        if start_date and end_date and end_date < start_date:
+            # Привязываем ошибку к конкретному полю, чтобы она отображалась рядом с ним
+            self.add_error('end_date', "Дата окончания контракта не может быть раньше даты его начала.")
+
+        return cleaned_data
